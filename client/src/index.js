f47660fec818e6c188924004f97f8a0a15d418d6
Jason Amerine <amerine.19@gmail.com>
2017-10-04 16:14:10 -0400

    changes

---
+++ client/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux'
+import store, { history } from './store/configureStore'
+import App from './components/app'
+import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+const target = document.querySelector('#root')
+
+render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <div>
+        <App />
+      </div>
+    </ConnectedRouter>
+  </Provider>,
+  target
+)
